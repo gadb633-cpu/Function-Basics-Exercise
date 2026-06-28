8010d2afcffb313751597c741d1a4a1fb34ece2a
gad-biton <gadb633@gmail.com>
2026-06-28 14:58:08 +0300

    added part_1 9

--- toolkit.py
+++ toolkit.py
@@ -44,3 +44,11 @@ def show_fahrenheit(c):
 show_fahrenheit(0)
 show_fahrenheit(100)
 show_fahrenheit(37.5)
+# 9
+def check_even(n):
+    if n %2 ==0:
+        print(f"{n} is even")
+    else:
+        print(f"{n} is odd")
+check_even(4)
+check_even(7)
