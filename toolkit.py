0ba1d3e8edcc0a40cf289e1d4a19f7e6facfbb42
gad-biton <gadb633@gmail.com>
2026-06-28 14:16:33 +0300

    added part_1 4

--- toolkit.py
+++ toolkit.py
@@ -1,7 +1,7 @@
 # part_1
 # 1
 def greet(name):
-    print("hello",name)
+    print("hello:",name)
 greet("agent X")
 # 2
 def add(a,b):
@@ -12,3 +12,11 @@ def square(n):
     print("square n is: ",n**2)
 square(5)
 square(12)
+# 4
+def greet_with_title(name,title="agent"):
+    print("hello:",title,name)
+greet_with_title(name="gad")
+greet_with_title(name="yossi",title="groom")
+# 5
+
+
